49fdfeb2fe6cbf9b71ad1872b205627ca132b295
rgriffit <rgriffit@student.42adel.org.au>
2024-05-29 12:54:50 +0930

    completed-fizzbuzz.c

---
+++ fizzbuzz/fizzbuzz.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rgriffit <rgriffit@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/29 11:18:25 by rgriffit          #+#    #+#             */
+/*   Updated: 2024/05/29 12:53:05 by rgriffit         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
+	else
+		ft_putchar(n + '0');
+}
+
+int	main(void)
+{
+	int	i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+			write(1, "fizzbuzz", 9);
+		else if ((i % 3) == 0)
+			write(1, "fizz", 5);
+		else if ((i % 5) == 0)
+			write(1, "buzz", 5);
+		else
+		ft_putnbr(i);
+		write(1, "\n", 1);
+		i++;
+	}
+	return (0);
+}
